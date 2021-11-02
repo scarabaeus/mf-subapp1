9e11008dfabbecfc5baa001fb6f537f87dacd75a
Steve Gomez <StevenAGomez@Live.com>
2021-11-02 13:01:24 -0700

    Initial demo commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
+import './public-path';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render(props) {
+  const { container } = props;
+  ReactDOM.render(<App />, container ? container.querySelector('#root') : document.querySelector('#root'));
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function storeTest(props) {
+  props.onGlobalStateChange((value, prev) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev), true);
+  props.setGlobalState({
+    ignore: props.name,
+    user: {
+      name: props.name,
+    },
+  });
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[mf-subapp1] react app bootstraped');
+}
+
+export async function mount(props) {
+  console.log('[mf-subapp1] props from main framework', props);
+  storeTest(props);
+  render(props);
+}
+
+export async function unmount(props) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'));
+}
+
