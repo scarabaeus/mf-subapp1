f3e2ea018b873b882bcf0cdb08bc83a491a66a18
Steve Gomez <StevenAGomez@Live.com>
2021-11-23 15:23:52 -0800

    Downgrades to react 16 for ebs components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useExperimental } from '@metromile-ebs/ebs-shared-ui-utils';
 import './App.css';
 
-const App = ({ standAlone = true }) => {
+const App = (props) => {
+  const { embedded = false } = props;
+  console.log('props', props);
+  const showExperimental = useExperimental();
   return (
     <div>
       mf-subapp1
-      <h2>Mode: {standAlone ? 'standalone' : 'embedded'}</h2>
+      <h2>Embedded Mode: {embedded ? 'Yes' : 'No'}</h2>
+      <h3>{showExperimental ? 'Experimental' : 'Not experimental'}</h3>
       <p>React version: {React.version}</p>
     </div>
   );
